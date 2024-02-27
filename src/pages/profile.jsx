94458d506b7355f87eb061cdd5cf461a2eb90998
Edwinestrada07 <r.1993andres.ee@gmail.com>
2024-02-27 14:20:49 -0500

    Creación del updateProfile

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import UpdateProfile from '../components/updateProfile';
 
 function UserProfile() {
     const [user, setUser] = useState(null);
@@ -19,7 +20,7 @@ function UserProfile() {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
-                    authorization: localStorage.getItem('token')
+                    authorization: token
                 }
             });
 
@@ -45,71 +46,8 @@ function UserProfile() {
             <p>Nombre: {user.name}</p>
             <p>Email: {user.email}</p>
             {/* Otros campos de información del usuario */}
-            <UpdateForm user={user} setUser={setUser} />
+            <UpdateProfile user={user} setUser={setUser} />
         </div>
-    );
-}
-
-function UpdateForm({ user, setUser }) {
-    const [formData, setFormData] = useState({
-        id: user.id,
-        name: user.name,
-        email: user.email
-    });
-
-    const handleChange = (e) => {
-        setFormData({
-            ...formData,
-            [e.target.name]: e.target.value
-        });
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {   
-            const token = localStorage.getItem('token');
-            if (!token) {
-                console.error("No se encontró token de autorización.");
-                return;
-            }
-    
-            const response = await fetch(`http://localhost:5000/user/${formData.id}`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json',
-                    authorization: `Bearer ${token}`
-                },
-                body: JSON.stringify(formData)
-            });
-    
-            if (!response.ok) {
-                throw new Error('No se pudo actualizar la información del usuario.');
-            }
-    
-            const responseData = await response.json();
-            console.log('Perfil actualizado:', responseData);
-            setUser(responseData);
-        } catch (error) {
-            console.error('Error al actualizar Perfil', error);
-            // Aquí podrías mostrar un mensaje de error al usuario
-        }
-    };
-    
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <h3>Actualizar Información</h3>
-            <label>
-                Nombre:
-                <input type="text" name="name" value={formData.name} onChange={handleChange} />
-            </label>
-            <label>
-                Email:
-                <input type="email" name="email" value={formData.email} onChange={handleChange} />
-            </label>
-            {/* Otros campos del formulario */}
-            <button type="submit">Actualizar</button>
-        </form>
     );
 }
 
