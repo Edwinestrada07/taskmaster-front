e90cac05e40cefeef389a184ec0622b6613d7c15
Edwinestrada07 <r.1993andres.ee@gmail.com>
2024-12-05 08:46:50 -0500

    update footer / assets screen

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
+import { FaGithub, FaLinkedin, FaPortrait } from "react-icons/fa"
 
 const Footer = () => {
     const location = useLocation();
@@ -9,22 +10,48 @@ const Footer = () => {
         return null;
     }
 
+    const socialInfo = [
+        {
+            icon: <FaLinkedin className="w-8 h-8" />,
+            href: "https://www.linkedin.com/in/edwinestradam/"
+        },
+        {
+            icon: <FaGithub className="w-8 h-8" />,
+            href: "https://github.com/Edwinestrada07"
+        },
+        {
+            icon: <FaPortrait className="w-8 h-8" />,
+            href: "https://portafolioedwinestrada.netlify.app/"
+        }
+    ]
+
     return (
-        <footer className="bg-gray-800 dark:bg-gray-800 text-white dark:text-gray-100 md:bg-transparent">
-            <div className="container p-4 flex flex-col lg:flex-row justify-between items-center">
-                <h5 className="text-lg mb-4 lg:mb-0">© 2024 Todos los derechos reservados</h5>
-                <ul className="list-inline flex space-x-4 mb-4 h-8 w-8">
-                    <li className="list-inline-item">
-                        <a href="https://github.com/Edwinestrada07" target="_blank" rel="noopener noreferrer">
-                            <i className="fab fa-github"></i>
-                        </a>
-                    </li>
-                    <li className="list-inline-item">
-                        <a href="https://www.linkedin.com/feed/" target="_blank" rel="noopener noreferrer">
-                            <i className="fab fa-linkedin"></i>
-                        </a>
-                    </li>
-                </ul>
+        <footer className="pt-10">
+            <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
+                <div className="justify-between sm:flex">
+                    <div className="space-y-6">
+                        <img
+                            src="./assets/logo.jpg"
+                            alt="Logo_T4skMaster"
+                            className="w-20 sm:mx-auto rounded-full shadow-xl"
+                        />
+                    </div>
+                    <div className="mt-6">
+                        {/* iconos redes */}
+                        <div className="flex items-center justify-center gap-x-3 text-gray-600 text-3xl mt-6 ">
+                            {
+                                socialInfo.map((item, idx) => (
+                                    <a key={idx} href={item.href} aria-label="social media" target="_blank" rel="noreferrer">
+                                        {item.icon}
+                                    </a>
+                                ))
+                            }
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-10 py-10 border-t md:text-center">
+                    <p className="text-gray-600 text-center">© 2024 Edwin Estrada. Todos los derechos reservados.</p>
+                </div>
             </div>
         </footer>
     )
